06e3675b0311fca974f76951cac5d6571e6bc338
andrew.wu <andrew.wu@andrewwu-HP>
2014-03-26 16:53:33 +0800

    h

--- src/com/metrics/MDOL/dao/LogFuncDao.java
+++
@@ -1,9 +0,0 @@
-package com.metrics.MDOL.dao;
-
-import com.metrics.MDOL.dbo.LogFunc;
-
-public interface LogFuncDao {
-	
-	public LogFunc getByName(String name);
-
-}
--- src/com/metrics/MDOL/dao/LogTypeDao.java
+++
@@ -1,9 +0,0 @@
-package com.metrics.MDOL.dao;
-
-import com.metrics.MDOL.dbo.LogType;
-
-public interface LogTypeDao {
-
-	public LogType getByName(String name);
-	
-}
